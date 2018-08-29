1f75577b9541f9b068055a231bc56d8d704dba15
motolola <motolola@icloud.com>
2018-08-29 22:59:08 +0100

    first commit

---
+++ src/main/java/io/motolola/standaloneactiveMQ/StandaloneActiveMqApplication.java
@@ -0,0 +1,12 @@
+package io.motolola.standaloneactiveMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneActiveMqApplication.class, args);
+	}
+}
